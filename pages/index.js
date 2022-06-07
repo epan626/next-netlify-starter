5f9ee847a8a76edfa9846906287e0b93ae73f1fe
epan626 <epan626@gmail.com>
2022-06-07 08:49:31 -0700

    added switch logic

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,30 @@ export default function Home() {
       <script
             dangerouslySetInnerHTML={{
               __html: `
+              const resume = document.getElementsByClassName('resumeContainer');
+              const highlight = document.getElementsByClassName('highlightContainer');
+              const aboutMe = document.getElementsByClassName('aboutMeContainer');
+              const meSwitch = document.getElementById('meSwitcher');
+              const locationText = document.getElementById('locationText');
+              const resumeSwitch = document.getElementById('resumeSwitcher');
               const switchThis = (e, text) => {
-                console.log(text);
+                if(text == "resume") {
+                  resume[0].style.display = "block";
+                  aboutMe[0].style.display = "none";
+                  resumeSwitch.style.background = "#8e8e8e";
+                  meSwitch.style.background = "#366f57";
+                  meSwitch.classList.remove("active");
+                  resumeSwitch.classList.add("active");
+                  locationText.innerText = "resume"
+                } else {
+                  aboutMe[0].style.display = "block"
+                  resume[0].style.display = "none";     
+                  meSwitch.style.background = "#8e8e8e";
+                  resumeSwitcher.style.background = "#366f57";
+                  resumeSwitcher.classList.remove("active");
+                  meSwitch.classList.add("active");
+                  locationText.innerText = "homepage"
+                }
               };
                   `,
             }}
